2e6fd1bd0070d93460fe6f34621be90a99056d14
Johan Pettersson <xsnudelx@hotmail.com>
2016-09-20 21:02:03 +0200

    renamed some variables and put proper indetentation

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 /* Labb 2 i DD1352 Algoritmer, datastrukturer och komplexitet    */
 /* Se labbanvisning under kurswebben https://www.kth.se/social/course/DD1352 */
 /* Ursprunglig författare: Viggo Kann KTH viggo@nada.kth.se      */
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,33 +11,32 @@ import java.util.List;
 
 public class Main {
 
-  public static List<String> readWordList(BufferedReader input) throws IOException {
-    LinkedList<String> list = new LinkedList<String>();
-    while (true) {
-      String s = input.readLine();
-      if (s.equals("#"))
-        break;
-      list.add(s);
+    public static List<String> readWordList(BufferedReader input) throws IOException {
+        LinkedList<String> list = new LinkedList<String>();
+        while (true) {
+            String s = input.readLine();
+            if (s.equals("#"))
+                break;
+            list.add(s);
+        }
+        return list;
     }
-    return list;
-  }
 
-  public static void main(String args[]) throws IOException {
-    //    long t1 = System.currentTimeMillis();
-    BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-    // Säkrast att specificera att UTF-8 ska användas, för vissa system har annan
-    // standardinställning för teckenkodningen.
-    List<String> wordList = readWordList(stdin);
-    String word;
-    while ((word = stdin.readLine()) != null) {
-      ClosestWords closestWords = new ClosestWords(word, wordList);
-      System.out.print(word + " (" + closestWords.getMinDistance() + ")");
-      for (String w : closestWords.getClosestWords())
-        System.out.print(" " + w);
-      System.out.println();
+    public static void main(String args[]) throws IOException {
+        long startTime = System.currentTimeMillis();
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
+        // Säkrast att specificera att UTF-8 ska användas, för vissa system har annan
+        // standardinställning för teckenkodningen.
+        List<String> wordList = readWordList(stdin);
+        String word;
+        while ((word = stdin.readLine()) != null) {
+            ClosestWords closestWords = new ClosestWords(word, wordList);
+            System.out.print(word + " (" + closestWords.getMinDistance() + ")");
+            for (String w : closestWords.getClosestWords())
+                System.out.print(" " + w);
+            System.out.println();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("CPU time: " + (endTime - startTime) + " ms");
     }
-    //    long tottime = (System.currentTimeMillis() - t1);
-    //    System.out.println("CPU time: " + tottime + " ms");
-
-  }
 }
